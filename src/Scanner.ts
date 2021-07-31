bb0917d744304f1292e83291cfe37eeb492da225
Max <golergka@gmail.com>
2021-07-31 05:54:18 +0300

    Chapter 4.6.1

--- src/Scanner.ts
+++ src/Scanner.ts
@@ -1,52 +1,120 @@
-import { Token } from "./Token";
-import { TokenType } from "./TokenType";
+import { report } from './lox'
+import { Token } from './Token'
+import { TokenType } from './TokenType'
 
 export class Scanner {
-    public constructor(
-        private readonly source: string
-    ) { }
+	public constructor(private readonly source: string) {}
 
-    private readonly tokens: Token[] = []
-    private start: number = 0
-    private current: number = 0
-    private line: number = 1
-    
-    isAtEnd() {
-        return this.current >= this.source.length
-    }
-    
-    scanToken() {
-        const c = this.advance()
-        switch (c) {
-            case '(': return this.addToken('LEFT_PAREN')
-            case ')': return this.addToken('RIGHT_PAREN')
-            case '{': return this.addToken('LEFT_BRACE')
-            case '}': return this.addToken('RIGHT_BRACE')
-            case ',': return this.addToken('COMMA')
-            case '.': return this.addToken('DOT')
-            case '-': return this.addToken('MINUS')
-            case '+': return this.addToken('PLUS')
-            case ';': return this.addToken('SEMICOLON')
-            case '*': return this.addToken('STAR')
-        }
-    }
+	private readonly tokens: Token[] = []
+	private start: number = 0
+	private current: number = 0
+	private line: number = 1
 
-    advance(): string {
-        return this.source.charAt(this.current++)
-    }
+	isAtEnd() {
+		return this.current >= this.source.length
+	}
 
-    addToken(token: TokenType, literal?: object) {
-        const text = this.source.substring(this.start, this.current)
-        this.tokens.push(new Token(token, text, literal, this.line))
-    }
-    
-    public scanTokens() {
-        while(!this.isAtEnd()) {
-            this.start = this.current
-            this.scanToken()
-        }
+	scanToken() {
+		const c = this.advance()
+		switch (c) {
+			case '(':
+				return this.addToken('LEFT_PAREN')
+			case ')':
+				return this.addToken('RIGHT_PAREN')
+			case '{':
+				return this.addToken('LEFT_BRACE')
+			case '}':
+				return this.addToken('RIGHT_BRACE')
+			case ',':
+				return this.addToken('COMMA')
+			case '.':
+				return this.addToken('DOT')
+			case '-':
+				return this.addToken('MINUS')
+			case '+':
+				return this.addToken('PLUS')
+			case ';':
+				return this.addToken('SEMICOLON')
+			case '*':
+				return this.addToken('STAR')
+			case '!':
+				return this.addToken(this.match('=') ? 'BANG_EQUAL' : 'BANG')
+			case '=':
+				return this.addToken(this.match('=') ? 'EQUAL_EQUAL' : 'EQUAL')
+			case '<':
+				return this.addToken(this.match('=') ? 'LESS_EQUAL' : 'LESS')
+			case '>':
+				return this.addToken(this.match('=') ? 'GREATR_EQUAL' : 'GREATER')
+			case '/':
+				if (this.match('/')) {
+					// A comment goes until the end of the line
+					while (this.peek() != '\n' && !this.isAtEnd()) this.advance()
+				} else {
+					this.addToken('SLASH')
+				}
+				break
+			case ' ':
+			case '\r':
+			case '\t':
+				// Ignore whitespace
+				break
+			case '\n':
+				this.line++
+				break
+			case '"':
+				return this.string()
+			default:
+				return report(this.line, 'Unexpected character')
+		}
+	}
+
+	string() {
+		while (this.peek() != '"' && this.isAtEnd()) {
+			if (this.peek() == '\n') this.line++
+			this.advance()
+		}
+
+		if (this.isAtEnd()) {
+			report(this.line, 'Unterminated string.')
+            return
+		}
         
-        this.tokens.push(new Token('EOF', '', null, this.line))
-        return this.tokens
-    }
+        // The closing "."
+        this.advance()
+        
+        // Trim the surrounding quotes.
+        const value = this.source.substring(this.start + 1, this.current - 1)
+        this.addToken('STRING', value)
+	}
+
+	peek() {
+		if (this.isAtEnd()) return '\0'
+		return this.source.charAt(this.current)
+	}
+
+	match(expected: string) {
+		if (this.isAtEnd()) return false
+		if (this.source.charAt(this.current) != expected) return false
+		this.current++
+		return true
+	}
+
+	advance(): string {
+		return this.source.charAt(this.current++)
+	}
+
+	addToken(token: TokenType, literal?: any) {
+		const text = this.source.substring(this.start, this.current)
+		this.tokens.push(new Token(token, text, literal, this.line))
+	}
+
+	public scanTokens() {
+		while (!this.isAtEnd()) {
+			this.start = this.current
+			this.scanToken()
+		}
+
+		this.tokens.push(new Token('EOF', '', null, this.line))
+		return this.tokens
+	}
 }
---
+++ src/lox.ts
@@ -0,0 +1,49 @@
+import * as fs from 'fs'
+import * as readline from 'readline'
+import { Scanner } from './Scanner'
+
+let hadError = false
+
+export function runFile(filename: string) {
+	const code = fs.readFileSync(filename, "utf8")
+	run(code, filename)
+	if (hadError) process.exit(1)
+}
+
+export async function runPrompt() {
+	const rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout,
+	})
+
+	async function question(query: string): Promise<string> {
+		return new Promise((resolve) => {
+			rl.question(query, resolve)	
+		})
+	}
+
+	while (true) {
+		const line = await question("> ")
+		if (line === null) break;
+		run(line)
+		hadError = false
+	}
+}
+
+function run(source: string) {
+	const scanner = new Scanner(source)
+	const tokens = scanner.scanTokens()
+	
+	for (const token of tokens) {
+		console.log(token)
+	}
+}
+
+export function report(line: number, message: string): void
+export function report(line: number, where: string, message: string): void
+export function report(line: number, arg1: string, arg2?: string) {
+	const where = arg2 ? arg1 : ''
+	const message = arg2 || arg1
+	console.log(`[line ${line}] Error${where}: ${message}`)
+	hadError = true
+}
