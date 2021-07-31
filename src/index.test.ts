533d17c8a70c54aacdfc1daa44a028bd7d33d40b
Max <golergka@gmail.com>
2021-07-31 04:49:30 +0300

    Copied template over

---
+++ src/index.test.ts
@@ -0,0 +1,5 @@
+describe('Test', () => {
+	it.concurrent('passes', async () => {
+		expect(1).toEqual(1)
+	})
+})
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+console.log('Hello, world')
+;(function () {
+	throw new Error('test error with Typescript stack trace')
+})()
