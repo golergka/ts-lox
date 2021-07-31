60f8a87eac43e2fba413a3965319502558fd3ff5
Max <golergka@gmail.com>
2021-07-31 05:39:21 +0300

    Chapter 4.5

--- src/Scanner.ts
+++ src/Scanner.ts
@@ -1,4 +1,5 @@
 import { Token } from "./Token";
+import { TokenType } from "./TokenType";
 
 export class Scanner {
     public constructor(
@@ -12,6 +13,31 @@ export class Scanner {
     
     isAtEnd() {
         return this.current >= this.source.length
+    }
+    
+    scanToken() {
+        const c = this.advance()
+        switch (c) {
+            case '(': return this.addToken('LEFT_PAREN')
+            case ')': return this.addToken('RIGHT_PAREN')
+            case '{': return this.addToken('LEFT_BRACE')
+            case '}': return this.addToken('RIGHT_BRACE')
+            case ',': return this.addToken('COMMA')
+            case '.': return this.addToken('DOT')
+            case '-': return this.addToken('MINUS')
+            case '+': return this.addToken('PLUS')
+            case ';': return this.addToken('SEMICOLON')
+            case '*': return this.addToken('STAR')
+        }
+    }
+
+    advance(): string {
+        return this.source.charAt(this.current++)
+    }
+
+    addToken(token: TokenType, literal?: object) {
+        const text = this.source.substring(this.start, this.current)
+        this.tokens.push(new Token(token, text, literal, this.line))
     }
     
     public scanTokens() {
